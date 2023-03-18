6ee1e36fc6a924dd8ac3a20f18a503c9d81ac0d6
Satoshi Kawamoto <satoshi.pes@gmail.com>
2023-03-19 03:07:52 +0900

    support for crinex ver 1.0

--- reader.go
+++ reader.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -27,10 +28,12 @@ func NewReader(r io.Reader) (io.Reader, error) {
 	)
 
 	// setup new crxReader
-	s, err := setup(r)
+	s, ver, err := setup(r)
 	if err != nil {
 		return r, err
 	}
+
+	_ = ver
 
 	// parse obsTypes and get all header contents
 	obsTypes, headers := scanHeader(s)
@@ -40,9 +43,33 @@ func NewReader(r io.Reader) (io.Reader, error) {
 		// update epoch record
 		epochStr = s.Text()
 		if strings.HasPrefix(epochStr, ">") {
+			// crinex ver 3.0
 			// check special event
 			if (len(epochStr) >= 35) && (epochStr[31] > '1') {
 				numSkip, err := strconv.Atoi(strings.TrimSpace(string(epochStr[32:35])))
+				if err == nil {
+					// special event found, skip numSkip lines
+					buf = append(buf, epochStr...)
+					buf = append(buf, '\n')
+					for i := 0; i < numSkip; i++ {
+						s.Scan()
+						buf = append(buf, s.Text()...)
+						buf = append(buf, '\n')
+					}
+					continue
+				} else {
+					// should be recover to the next epoch record that begins with '>'.
+				}
+			}
+
+			// initialize epoch record
+			epochRec.buf = []byte(epochStr)
+			data = make(map[string]satDataRecord)
+		} else if strings.HasPrefix(epochStr, "&") {
+			// crinex ver 1.0
+			// check special event
+			if (len(epochStr) >= 32) && (epochStr[28] > '1') {
+				numSkip, err := strconv.Atoi(strings.TrimSpace(string(epochStr[29:32])))
 				if err == nil {
 					// special event found, skip numSkip lines
 					buf = append(buf, epochStr...)
@@ -71,7 +98,12 @@ func NewReader(r io.Reader) (io.Reader, error) {
 		clk.Update([]byte(clockStr))
 
 		// get list of satellites
-		satList := getSatList(epochRec.Bytes())
+		var satList []string
+		if ver == "3.0" {
+			satList = getSatList(epochRec.Bytes())
+		} else if ver == "1.0" {
+			satList = getSatListV1(epochRec.Bytes())
+		}
 
 		// read data block
 		for _, satId := range satList {
@@ -84,7 +116,11 @@ func NewReader(r io.Reader) (io.Reader, error) {
 
 			// allocate for new sat
 			if _, ok := data[satId]; !ok {
-				data[satId] = NewSatDataRecord(obsCodes)
+				if ver == "3.0" {
+					data[satId] = NewSatDataRecord(obsCodes)
+				} else if ver == "1.0" {
+					data[satId] = NewSatDataRecordV1(obsCodes)
+				}
 			}
 
 			// Update code and phase data
@@ -100,6 +136,12 @@ func NewReader(r io.Reader) (io.Reader, error) {
 
 				b := []byte(vals[j])
 				dj.Update(b)
+
+				// initialize arc
+				if ver == "1.0" && len(b) > 1 && b[1] == '&' {
+					data[satId].lli[j].buf[0] = ' '
+					data[satId].ss[j].buf[0] = ' '
+				}
 			}
 
 			// Update LLI and SS
@@ -123,31 +165,65 @@ func NewReader(r io.Reader) (io.Reader, error) {
 		// ----- CRX to RINEX -----
 		// buffer data in the RINEX format
 		// epoch record
-		if clk.missing {
-			buf = append(buf, fmt.Sprintf("%-35.35s\n", epochRec.StringRINEX())...)
-		} else {
-			buf = append(buf, fmt.Sprintf("%-35.35s      %15.12f\n", epochRec.StringRINEX(), float64(clk.refData)*0.000000000001)...)
-		}
 
-		// data block
-		for _, satId := range satList {
-			var bufs []byte
-			bufs = append(bufs, fmt.Sprintf("%3.3s", satId)...)
-
-			d := data[satId]
-			for k, d1 := range d.data {
-				if d1.missing {
-					bufs = append(bufs, "                "...)
-					continue
-				}
-				//bufs = append(bufs, fmt.Sprintf("%14.3f%1c%1c", float64(ref)*0.001, d.lli[k].buf[0], d.ss[k].buf[0])...)
-				bufs = append(bufs, intToRinexDataBytes(d1.refData)...)
-				bufs = append(bufs, d.lli[k].buf[0])
-				bufs = append(bufs, d.ss[k].buf[0])
+		if ver == "3.0" {
+			// epoch record
+			if clk.missing {
+				buf = append(buf, fmt.Sprintf("%-35.35s\n", epochRec.StringRINEX())...)
+			} else {
+				buf = append(buf, fmt.Sprintf("%-35.35s      %15.12f\n", epochRec.StringRINEX(), float64(clk.refData)*0.000000000001)...)
 			}
-			buf = append(buf, bytes.TrimRight(bufs, " ")...)
-			buf = append(buf, '\n')
+
+			// data block
+			for _, satId := range satList {
+				var bufs []byte
+				bufs = append(bufs, fmt.Sprintf("%3.3s", satId)...)
+
+				d := data[satId]
+				for k, d1 := range d.data {
+					if d1.missing {
+						bufs = append(bufs, "                "...)
+						continue
+					}
+					//bufs = append(bufs, fmt.Sprintf("%14.3f%1c%1c", float64(ref)*0.001, d.lli[k].buf[0], d.ss[k].buf[0])...)
+					bufs = append(bufs, intToRinexDataBytes(d1.refData)...)
+					bufs = append(bufs, d.lli[k].buf[0])
+					bufs = append(bufs, d.ss[k].buf[0])
+				}
+				buf = append(buf, bytes.TrimRight(bufs, " ")...)
+				buf = append(buf, '\n')
+			}
+		} else if ver == "1.0" {
+			if clk.missing {
+				buf = append(buf, epochRec.StringRINEXV2(math.NaN())...)
+			} else {
+				buf = append(buf, epochRec.StringRINEXV2(float64(clk.refData)*0.000000001)...)
+			}
+
+			// data block
+			for _, satId := range satList {
+				var bufs []byte
+
+				d := data[satId]
+				for k, d1 := range d.data {
+					if d1.missing {
+						bufs = append(bufs, "                "...)
+					} else {
+						bufs = append(bufs, intToRinexDataBytes(d1.refData)...)
+						bufs = append(bufs, d.lli[k].buf[0])
+						bufs = append(bufs, d.ss[k].buf[0])
+					}
+
+					// line feed
+					if k == len(d.data)-1 || (k+1)%5 == 0 {
+						buf = append(buf, bytes.TrimRight(bufs[:], " ")...)
+						buf = append(buf, '\n')
+						bufs = []byte{}
+					}
+				}
+			}
 		}
+
 	}
 
 	return bytes.NewReader(buf), nil
@@ -211,6 +287,17 @@ func getSatList(b []byte) []string {
 	return satList
 }
 
+func getSatListV1(b []byte) []string {
+	satList := []string{}
+	for i := 32; i+3 <= len(b); i += 3 {
+		satId := string(b[i : i+3])
+		if satId != "   " {
+			satList = append(satList, satId)
+		}
+	}
+	return satList
+}
+
 // strRecord stores the previous epoch record string.
 //
 // call strRecord.Update(newString) to update to the current epoch.
@@ -233,6 +320,43 @@ func (e *strRecord) StringRINEX() string {
 		s = strings.TrimRight(s, " ")
 	}
 	return s
+}
+
+func (e *strRecord) StringRINEXV2(clk float64) string {
+	var b []byte
+
+	numSat, err := strconv.Atoi(string(bytes.TrimSpace(e.buf[29:32])))
+	_ = err
+
+	// first line
+	if numSat > 12 {
+		if math.IsNaN(clk) {
+			// clock is missing
+			b = append(b, fmt.Sprintf(" %67s\n", string(e.buf[1:68]))...)
+		} else {
+			b = append(b, fmt.Sprintf(" %67s%12.9f\n", string(e.buf[1:68]), clk)...)
+		}
+	} else {
+		if math.IsNaN(clk) {
+			// clock is missing
+			b = append(b, fmt.Sprintf(" %s\n", e.buf[1:32+3*numSat])...)
+		} else {
+			b = append(b, fmt.Sprintf(" %-67s%12.9f\n", e.buf[1:32+3*numSat], clk)...)
+		}
+		return string(b)
+
+	}
+
+	// continuation lines
+	for i := 1; numSat > 12*i; i++ {
+		if numSat >= 12*(i+1) {
+			b = append(b, fmt.Sprintf("%32s%-36.36s\n", "", e.buf[32+36*i:32+36*(i+1)])...)
+		} else {
+			b = append(b, fmt.Sprintf("%32s%-s\n", "", e.buf[32+36*i:32+36*i+3*(numSat%12)])...)
+		}
+	}
+
+	return string(b)
 }
 
 func (e *strRecord) Update(s string) error {
@@ -278,6 +402,26 @@ func NewSatDataRecord(obsCodes []string) satDataRecord {
 		lli:      make([]strRecord, len(obsCodes)),
 		ss:       make([]strRecord, len(obsCodes)),
 	}
+}
+
+// NewSatDataRecord returns a new satDataRecord initialized with obsCodes.
+func NewSatDataRecordV1(obsCodes []string) satDataRecord {
+	r := satDataRecord{
+		obsCodes: obsCodes,
+		data:     make([]diffRecord, len(obsCodes)),
+		lli:      make([]strRecord, len(obsCodes)),
+		ss:       make([]strRecord, len(obsCodes)),
+	}
+
+	// for crinex version 1
+	// Initialize LLI and SS because no initialization identifier is defined
+	// in the crinex version 1.
+	for i := 0; i < len(obsCodes); i++ {
+		r.lli[i].buf = []byte{' '}
+		r.ss[i].buf = []byte{' '}
+	}
+
+	return r
 }
 
 type diffRecord struct {
@@ -344,10 +488,10 @@ func (r *diffRecord) Update(b []byte) error {
 	return nil
 }
 
-func setup(r io.Reader) (*bufio.Scanner, error) {
-	s := bufio.NewScanner(r)
-	if err := s.Err(); err != nil {
-		return s, err
+func setup(r io.Reader) (s *bufio.Scanner, ver string, err error) {
+	s = bufio.NewScanner(r)
+	if err = s.Err(); err != nil {
+		return s, ver, err
 	}
 
 	// check first line: "CRINEX VERS   / TYPE"
@@ -357,45 +501,58 @@ func setup(r io.Reader) (*bufio.Scanner, error) {
 
 	// check header
 	if len(t) < 40 {
-		return s, ErrBadMagic
+		return s, ver, ErrBadMagic
 	}
 
-	ver := strings.TrimSpace(t[:20])
+	ver = strings.TrimSpace(t[:20])
 	magic := t[20:40]
 
 	//3.0                 COMPACT RINEX FORMAT                    CRINEX VERS   / TYPE
 	if magic != "COMPACT RINEX FORMAT" {
-		return s, ErrBadMagic
+		return s, ver, ErrBadMagic
 	}
-	if ver != "3.0" {
-		return s, ErrNotSupportedVersion
+	if ver != "3.0" && ver != "1.0" {
+		return s, ver, ErrNotSupportedVersion
 	}
 
 	// skip second line: "CRINEX PROG / DATE"
 	s.Scan()
 
-	return s, nil
+	return s, ver, nil
 }
 
 func scanHeader(s *bufio.Scanner) (obsTypes map[string][]string, h []byte) {
 	var obsTypesStrings []string
+	var obsTypesStringsV2 []string
+	var rinexVer byte
 
 	for s.Scan() {
 		buf := s.Text()
 		h = append(h, []byte(buf)...)
 		h = append(h, byte('\n'))
 
+		if strings.HasPrefix(buf[60:], "RINEX VERSION / TYPE") {
+			rinexVer = strings.TrimSpace(buf[:20])[0] // '2', '3', or '4'
+		}
 		if strings.HasPrefix(buf[60:], "SYS / # / OBS TYPES") {
 			obsTypesStrings = append(obsTypesStrings, buf)
 		}
-
+		if strings.HasPrefix(buf[60:], "# / TYPES OF OBSERV") {
+			obsTypesStringsV2 = append(obsTypesStringsV2, buf)
+		}
 		if strings.HasPrefix(buf[60:], "END OF HEADER") {
 			break
 		}
 	}
 
 	// currently errors are ignored
-	obsTypes, _ = parseObsTypes(obsTypesStrings)
+	if rinexVer >= '3' {
+		obsTypes, _ = parseObsTypes(obsTypesStrings)
+	} else if rinexVer >= '2' {
+		obsTypes, _ = parseObsTypesV2(obsTypesStringsV2)
+	} else {
+		// not supported
+	}
 
 	return
 }
@@ -446,5 +603,72 @@ func parseObsTypes(buf []string) (obsTypes map[string][]string, err error) {
 			}
 		}
 	}
+	return
+}
+
+func parseObsTypesV2(buf []string) (obsTypes map[string][]string, err error) {
+	var (
+		s        string
+		numCodes int
+	)
+	obsTypes = make(map[string][]string)
+
+	if len(buf) == 0 {
+		err = fmt.Errorf("failed to parse obsTypes, no data found")
+		return
+	}
+
+	s = buf[0]
+	sep := strings.Fields(s)
+	sep = sep[1:] // remove the first element that indicates the numCodes
+
+	// parse number of obsCodes
+	numCodes, err = strconv.Atoi(strings.TrimSpace(s[:6]))
+	if err != nil {
+		err = fmt.Errorf("failed to parse numCodes, s='%s', err=%v", s[:6], err)
+		return
+	}
+	obsCodes := make([]string, numCodes)
+
+	for k := 0; k < len(buf); k++ {
+		// s = buf[k]
+
+		// if len(s) < 6 {
+		// 	err = fmt.Errorf("too short msg, s='%s'", s)
+		// 	return
+		// }
+
+		n := 0    // number of codes in the current line
+		idx := 10 // index of the string
+		for i := 0; i < numCodes; i++ {
+			if len(sep[n]) >= 2 {
+				obsCodes[i] = sep[n][:2]
+			} else {
+				err = fmt.Errorf("failed to parse obsCode, s='%s'", sep[n])
+				return
+			}
+
+			n++
+			idx += 6
+			if n == 9 && i+1 < numCodes {
+				// move to the new line
+				k++
+				s = buf[k]
+				n, idx = 0, 10
+
+				// workaround for invalid format
+				if len(s) > 60 {
+					sep = strings.Fields(s[:60])
+				} else {
+					sep = strings.Fields(s)
+				}
+			}
+		}
+	}
+
+	for _, satSys := range []string{"G", "R", "J", "E", "C", "I", "S"} {
+		obsTypes[satSys] = obsCodes
+	}
+
 	return
 }
